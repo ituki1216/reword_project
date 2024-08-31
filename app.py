9df046c0c6096a99fbdde34fcab11f7543466f40
ituki yamanaka <yamanakasan@icloud.com>
2024-08-31 13:20:12 +0900

    T

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello dWorld!'
+    return 'Hello AAAAAAWorld!'
 
 
 if __name__ == '__main__':
