6653edb398a4acd4a8c48959a19007f3c23f8d20
KakutaAtsushi <kakuta.atsushi0909@gmail.com>
2024-08-31 21:01:05 +0900

    db init

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
-from sqlalchemy import create_engine
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Reword(db.Model):
-    __tablename__ = 'rewords'
     id = db.Column(db.Integer, primary_key=True)
-    reword = db.Column(db.String(200), nullable=False)
-
-
-def create_tables():
-    db.create_all()
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
 
 
 @app.route('/')
-def hello_world(): 
-    data = Reword.query.all()
-    return render_template('index.html', date=data)
+def hello_world():
+    data = Reword.query.all();
+    return render_template('index.html',date=data)
+
 
 @app.route('/add', methods=['POST'])
 def add():
-	reword_text = request.form['reword']
-	new_reword = Reword(reword=reword_text)
-	db.session.add(new_reword)
-	db.session.commit()
-	return redirect(url_for('index'))
+    reword_text = request.form['reword']
+    new_reword = Reword(reword=reword_text)
+    db.session.add(new_reword)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
