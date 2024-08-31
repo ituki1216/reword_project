d1de99341ef092adf36981275d446a136862ad77
KakutaAtsushi <kakuta.atsushi0909@gmail.com>
2024-08-31 20:51:10 +0900

    featurefront

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello BBBBBWorld!'
+def hello_world():
+    return render_template("home/index.html")
 
 
 if __name__ == '__main__':
