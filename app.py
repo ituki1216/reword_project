a289c7ff8c1d36676fb4754a5041f2b9f2099600
ituki yamanaka <yamanakasan@icloud.com>
2024-09-01 20:11:55 +0900

    たくさん機能つくりました

--- app.py
+++ app.py
@@ -14,21 +14,47 @@ migrate = Migrate(app, db)
 class Reword(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    reword_kind = db.Column(db.Boolean)
     description = db.Column(db.String(200))
 
-@app.route('/')
+@app.route('/add', methods=['GET'])
 def hello_world():
-    data = Reword.query.all()
-    return render_template('register_rewords/index.html',data=data)
+    small_reword = Reword.query.filter(Reword.reword_kind == 0)
+    big_reword = Reword.query.filter(Reword.reword_kind == 1)
+    return render_template('register_rewords/index.html',small_reword=small_reword, big_reword=big_reword)
 
+@app.route('/')
+def Home():
+    return render_template('home/index.html')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    id = request.form["id"]
+    record_to_delete = Reword.query.filter_by(id=id).first()
+    db.session.delete(record_to_delete)
+    db.session.commit()
+    return redirect("/add")
+
+@app.route('/update', methods=['POST'])
+def update():
+    id = request.form["id"]
+    name = request.form["reword"]
+    reword = Reword.query.filter_by(id=id).first()
+    reword.name = name
+    db.session.commit()
+    return redirect("/add")
 
 @app.route('/add', methods=['POST'])
 def add():
+    reword_kind = False
+    print(request.form)
+    if  request.form.get('reword_kind'):
+        reword_kind = True
     reword_text = request.form['reword']
-    new_reword = Reword(name = reword_text)
+    new_reword = Reword(name = reword_text, reword_kind = reword_kind)
     db.session.add(new_reword)
     db.session.commit()
-    return redirect("/")
+    return redirect("/add")
 
 
 if __name__ == '__main__':
