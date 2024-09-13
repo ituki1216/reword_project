4b33b8d1e060da97e63a56bf2c726e0d9c10b365
ituki yamanaka <yamanakasan@icloud.com>
2024-09-14 01:57:28 +0900

    login機能を作りました

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 import json
 from datetime import timedelta
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, flash, url_for
 from flask import request, redirect, session
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import random 
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = random.random()
 app.secret_key = 'timer'
 app.permanent_session_lifetime = timedelta(minutes=5)  # -> 5分 #(days=5) -> 5日保存
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 migrate = Migrate(app, db)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True) #primaryは連番かつ一意な値 データベースの**主キー（primary key）**として設定されたフィールドが、各レコードごとに一意な（他のレコードと重複しない）値を持ち、さらにその値が連続して増加することを指します。以下に詳しく説明します。
+    name = db.Column(db.String(30), nullable=False)
+    mail_address = db.Column(db.String(140), nullable=False, unique=True) #uniqueデータベースのフィールドに対して一意制約を設けるためのオプションです。これにより、そのフィールドの値が他のレコードと重複しないように強制されます
+    password = db.Column(db.String(120)) #hash化する可能性ある１２０
 
 
 class UserPoints(db.Model):
@@ -32,6 +47,7 @@ points = 0
 
 
 @app.route('/')
+@login_required
 def Home():
     small_reword_arr = []
     big_reword_arr = []
@@ -43,6 +59,39 @@ def Home():
         big_reword_arr.append(data.name)
     return render_template('home/index.html', small_reword=json.dumps(small_reword_arr), big_reword=big_reword_arr,
                            points=UserPoints)
+
+@app.route('/signup', methods=['GET'])
+def sign():
+    return render_template('register_rewords/signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+        username = request.form.get('name')        
+        password = request.form.get('password')
+        mail_address = request.form.get('mail_address')
+        user = User(name=username, mail_address=mail_address, password=generate_password_hash(password))
+        db.session.add(user)
+        db.session.commit()
+        return redirect('login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(mail_address=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('Home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+            print("test")
+    return render_template('register_rewords/login.html') 
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route("/", methods=["GET"])
