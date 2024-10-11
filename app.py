d89007966152b9b6f1d86d82d73212d706472f17
ituki yamanaka <yamanakasan@icloud.com>
2024-10-12 08:47:51 +0900

    update

--- app.py
+++ app.py
@@ -307,8 +307,8 @@ def GoHome():
 
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=True)
 
 
 
